9aa34fc6de718885f70400b74ae38ebb36295c8c
Eduardo Silva <edsiper@gmail.com>
2012-09-24 09:39:29 -0600

    Example 050: Cleanup websocket chat example
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- 050_websocket_chat/main.c
+++ 050_websocket_chat/main.c
@@ -3,16 +3,11 @@
 #include "webservice.h"
 #include "packages/websocket/websocket.h"
 
-DUDA_REGISTER("Websockets Service Example", "service");
+DUDA_REGISTER("Duda I/O Examples", "WebSocket Chat");
 
 void cb_on_message(duda_request_t *dr, ws_request_t *wr)
 {
     websocket->broadcast(wr, wr->payload, wr->payload_len, WS_OPCODE_TEXT);
-}
-
-void cb_on_close(duda_request_t *dr, struct ws_request *wr)
-{
-    printf("on close!!!!!!!!!!\n");
 }
 
 void cb_handshake(duda_request_t *dr)
@@ -20,36 +15,21 @@ void cb_handshake(duda_request_t *dr)
     websocket->handshake(dr);
 }
 
-void cb_hola(duda_request_t *dr)
-{
-    printf("asdasd\n");
-}
-
 int duda_main()
 {
-    duda_interface_t *if_system;
-    duda_method_t    *method;
-
+    /* Load the websocket package */
     duda_load_package(websocket, "websocket");
+
+    /*
+     * Define a callback, on every websocket message received,
+     * trigger cb_on_message.
+     */
     websocket->set_callback(WS_ON_MESSAGE, cb_on_message);
-    websocket->set_callback(WS_ON_CLOSE, cb_on_close);
 
+    /* Associate a static URL with a callback */
+    map->static_add("/handshake/", "cb_handshake");
 
-    map->static_add("/hola", "cb_hola");
-
-
-    /* archive interface */
-    if_system = map->interface_new("example");
-
-    /* URI: /websockets/example/handshake  */
-    method = map->method_new("handshake", "cb_handshake", 0);
-    map->interface_add_method(method, if_system);
-
-    /* Add interface to map */
-    map->add_interface(if_system);
-    //duda_service_add_interface(if_system);
-
-    /* Initialize the broadcast interface */
+    /* Initialize the broadcaster interface */
     websocket->broadcaster();
 
     return 0;
