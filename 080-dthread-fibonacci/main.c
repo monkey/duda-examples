67afc08e14cc29f763d68ebd0fde5bb4df7cda0e
Eduardo Silva <eduardo@monkey.io>
2014-08-01 16:49:12 -0600

    008-dthreads-fibonacci: code cleanup
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- 080-dthread-fibonacci/main.c
+++ 080-dthread-fibonacci/main.c
@@ -1,3 +1,5 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
 #include "webservice.h"
 
 DUDA_REGISTER("Dthread Fibonacci Examples", "dthread fibonacci demo");
@@ -12,23 +14,26 @@ void consumer(void *data)
     struct bundle *bdl = data;
     duda_dthread_channel_t *chan = bdl->chan;
     duda_request_t *dr = bdl->dr;
+
     response->http_status(dr, 200);
+
     while (!dthread->chan_done(chan)) {
         int *n = dthread->chan_recv(chan);
         response->printf(dr, "%d\n", *n);
-        monkey->mem_free(n);
+        mem->free(n);
     }
     response->end(dr, NULL);
 }
 
-void productor(void *data)
+void producer(void *data)
 {
-    duda_dthread_channel_t *chan = data;
+    int i;
     int num1 = 1;
     int num2 = 1;
-    int i;
+    duda_dthread_channel_t *chan = data;
+
     for (i = 0; i < 20; ++i) {
-        int *n = monkey->mem_alloc(sizeof(int));
+        int *n = mem->alloc(sizeof(int));
         if (i == 0) {
             *n = num1;
         } else if (i == 1) {
@@ -45,18 +50,22 @@ void productor(void *data)
 
 void cb_fibonacci(duda_request_t *dr)
 {
-    // unbuffered channel
-    duda_dthread_channel_t *chan = dthread->chan_create(0, monkey->mem_free);
-    struct bundle *bdl = monkey->mem_alloc(sizeof(*bdl));
+    int cid;
+    int pid;
+    duda_dthread_channel_t *chan = dthread->chan_create(0, mem->free);
+    struct bundle *bdl = mem->alloc(sizeof(*bdl));
+
     bdl->chan = chan;
     bdl->dr = dr;
-    int cid = dthread->create(consumer, bdl);
-    int pid = dthread->create(productor, chan);
+
+    cid = dthread->create(consumer, bdl);
+    pid = dthread->create(producer, chan);
+
     dthread->chan_set_sender(chan, pid);
     dthread->chan_set_receiver(chan, cid);
     dthread->resume(cid);
     dthread->chan_free(chan);
-    monkey->mem_free(bdl);
+    mem->free(bdl);
 }
 
 int duda_main()
